9b44368f1a7ba6d64b35aa8177b68f13866c306d
Michael de Lang <kingoipo@gmail.com>
2019-09-17 11:41:01 +0200

    Add missing CFLAG for external components

--- compile_external_release.sh
+++ compile_external_release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CPUS="$( grep -c ^processor /proc/cpuinfo )"
-export CFLAGS="-DXXH_CPU_LITTLE_ENDIAN=1 -DNDEBUG -mavx"
-export CXXFLAGS="-DXXH_CPU_LITTLE_ENDIAN=1 -DNDEBUG -mavx"
+export CFLAGS="-DXXH_CPU_LITTLE_ENDIAN=1 -DNDEBUG -mavx -O2"
+export CXXFLAGS="-DXXH_CPU_LITTLE_ENDIAN=1 -DNDEBUG -mavx -O2"
 
 cd $DIR/external/libsodium
 ./autogen.sh
